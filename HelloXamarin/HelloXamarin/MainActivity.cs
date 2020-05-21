b46f1f6a26a924fe5b6fd785bbba74e885916edd
NAPTlME <napratt2@yahoo.com>
2020-05-20 23:16:06 -0500

    Initial push of Xamarin tutorial

---
+++ HelloXamarin/HelloXamarin/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace HelloXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += delegate { button.Text = "Hello world, I am your first App"; };
+        }
+    }
+}
